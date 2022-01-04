0c6acdbe3eaa9c98c4952c59b3ad86d27cfe2a84
afonsorn <aro.nascimento@campus.fct.unl.pt>
2022-01-04 18:20:07 +0000

    Added GlobalMethodSecurity to enable Policies

--- bookshelf/src/main/kotlin/pt/unl/fct/di/iadidemo/bookshelf/config/SecurityConfig.kt
+++ bookshelf/src/main/kotlin/pt/unl/fct/di/iadidemo/bookshelf/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package pt.unl.fct.di.iadidemo.bookshelf.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -9,6 +10,7 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import pt.unl.fct.di.iadidemo.bookshelf.application.services.UserService
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 class SecurityConfig(
     val customUserDetails:CustomUserDetailsService,
     val users: UserService
